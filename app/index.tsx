2714e10d00ad8937b6ee0530505e0e7519f6d36b
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-12-09 10:10:31 +0100

    Piccole aggiunte di layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,37 @@
-import { Image, Text, View } from 'react-native';
+import { Image, ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 import VerticalLogo from '@/components/molecules/verticalLogo/verticalLogo.molecule';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <VerticalLogo />
-    </View>
+    <SafeAreaProvider>
+      <ImageBackground
+        source={require('../assets/images/appImages/onBoard1.png')}
+        resizeMode="contain"
+        style={{ flex: 1, justifyContent: 'flex-end' }}>
+        <View
+          style={{
+            height: 250,
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'orange',
+            borderTopEndRadius: 40,
+            borderTopStartRadius: 40,
+            padding: 20,
+          }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'purple',
+              width: '100%',
+              alignItems: 'center',
+              padding: 10,
+              borderRadius: 20,
+            }}>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 25 }}>Get Started!</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </SafeAreaProvider>
   );
 }
