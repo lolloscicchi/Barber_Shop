cf91c09c9f41af7c209fa27f8b1879c07c49a418
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-12-12 12:35:22 +0100

    wip: login

--- app/screens/login/login.tsx
+++ app/screens/login/login.tsx
@@ -1,19 +1,27 @@
 import { styles } from '@/components/molecules/verticalLogo/verticalLogo.styles';
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, ImageBackground, Image } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function Login() {
   return (
     <SafeAreaProvider>
-        <ImageBackground style={{
+      <ImageBackground
+        style={{
           backgroundColor: '#F99417',
           flex: 1,
           alignItems: 'center',
-          justifyContent: 'center',
-          paddingTop:0
-        }} source={require("@/assets/images/appImages/background/backgroundIcons.png")} resizeMode='contain' >
-      
-        <Text>LOGIN</Text>
+          justifyContent: 'flex-end',
+          paddingTop: 0,
+        }}
+        source={require('@/assets/images/appImages/background/backgroundIcons.png')}
+        resizeMode="cover">
+        <Image
+          style={{ flex: 1, width: 200 }}
+          source={require('@/assets/images/appImages/login/loginBarber.png')}
+        />
+        <View style={{ flex: 2, backgroundColor: 'white', width: '100%' }}>
+          <Text>LOGIN</Text>
+        </View>
       </ImageBackground>
     </SafeAreaProvider>
   );
